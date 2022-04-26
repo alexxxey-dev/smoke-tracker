ea906964b90df1566430cbc594908cdd25c43d30
goodWtuff <vzkzf0cc>
2022-04-26 18:08:26 +0300

    break time updated

--- app/src/main/java/com/eatmybrain/smoketracker/util/BreakCalculator.kt
+++ app/src/main/java/com/eatmybrain/smoketracker/util/BreakCalculator.kt
@@ -14,11 +14,11 @@ object BreakCalculator {
 
 
         return when (sessionsPerWeek) {
-            in 99.downTo(21) -> if (smokeAmount > 0.5) week * 7 else week * 6
-            in 21.downTo(14) -> if (smokeAmount > 0.5) week * 6 else week * 5
-            in 14.downTo(7) -> if (smokeAmount > 0.5) week * 5 else week * 4
-            in 7.downTo(5) -> if (smokeAmount > 0.5) week * 4 else week * 2
-            in 5.downTo(3) -> if (smokeAmount > 0.5) week * 3 else week * 2
+            in 99.downTo(21) -> if (smokeAmount > 0.5) week * 12 else week * 11
+            in 21.downTo(14) -> if (smokeAmount > 0.5) week * 10 else week * 9
+            in 14.downTo(7) -> if (smokeAmount > 0.5) week * 8 else week * 7
+            in 7.downTo(5) -> if (smokeAmount > 0.5) week * 6 else week * 5
+            in 5.downTo(3) -> if (smokeAmount > 0.5) week * 4 else week * 3
             else -> week * 2
         }
     }
